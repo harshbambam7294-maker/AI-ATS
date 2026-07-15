cafdd45e070aefdc64f53a7b95db1d4c037b9dd4
HarshBambam <harshbambam7294@gmail.com>
2026-07-15 18:38:20 +0530

    Fixed other deployment issues

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
 });
 
+export default api;
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
