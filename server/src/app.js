4340e34478b36a7ec996db0d7df4e0529f0487cd
HarshBambam <harshbambam7294@gmail.com>
2026-07-15 18:30:16 +0530

    frontend-backend deployment cors resolved

--- server/src/app.js
+++ server/src/app.js
@@ -8,13 +8,28 @@ const app = express();
 // Middleware
 // ----------------------
 
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:5174"
-    ],
-    credentials: true
-}));
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    process.env.CLIENT_URL
+];
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+
+            // Allow Postman, mobile apps, server-to-server requests
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+
+            return callback(new Error("Not allowed by CORS"));
+        },
+        credentials: true
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
